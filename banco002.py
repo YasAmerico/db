f0a22ab302130840f4ebe6cbaa9050e6be867171
YasAmerico <166563849+YasAmerico@users.noreply.github.com>
2024-06-17 21:23:47 -0300

    Update banco002.py

--- banco002.py
+++ banco002.py
@@ -29,3 +29,37 @@ for user in user:
 
 userChris = User.get(nome="Chris")
 print(userChris.nome,userChris.idade)
+
+#outra forma de fazer
+from peewee import SqliteDatabase,Model,IntegerField,CharField
+
+#configuração do banco de dados
+db = SqliteDatabase('example1.db')
+
+#definição do modelo
+class User(Model):
+    nome = CharField()
+    idade = IntegerField ()
+
+    class Meta:
+        database = db
+
+#criação das tabelas
+db.connect()
+db.create_tables([User])
+
+#adição de um novo usuário
+User.create(nome='Chris',idade =27)
+User.create(nome='Felix',idade =24)
+User.create(nome='I.n',idade =23)
+User.create(nome='Minho',idade =26)
+
+#função para consultar registros por nome e idade
+def encontrar_usuario_por_nome_idade(nome,idade):
+    query =User.select().where((User.nome == nome)& (User.idade == idade))
+    return query
+
+#consulta de usuarios
+users =encontrar_usuario_por_nome_idade('Felix',24)
+for user in users:
+    print(f'Name:{user.nome},Idade:{user.idade}')
